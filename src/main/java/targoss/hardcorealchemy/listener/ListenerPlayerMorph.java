dc3dd0195208c6dabfbdec3e19f8208cd77b93e3
asanetargoss <asanetargoss@users.noreply.github.com>
2017-09-02 22:17:52 -0700

    Prevent acquiring human morphs; not sure why this diff is so bad

--- src/main/java/targoss/hardcorealchemy/listener/ListenerPlayerMorph.java
+++ src/main/java/targoss/hardcorealchemy/listener/ListenerPlayerMorph.java
@@ -1,7 +1,9 @@
 package targoss.hardcorealchemy.listener;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import ladysnake.dissolution.common.capabilities.CapabilityIncorporealHandler;
 import ladysnake.dissolution.common.capabilities.IIncorporealHandler;
@@ -11,6 +13,7 @@ import mchorse.metamorph.api.events.SpawnGhostEvent;
 import mchorse.metamorph.api.morphs.AbstractMorph;
 import mchorse.metamorph.capabilities.morphing.IMorphing;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.EnumFacing;
@@ -33,64 +36,68 @@ import targoss.hardcorealchemy.capability.killcount.CapabilityKillCount;
 import targoss.hardcorealchemy.capability.killcount.ICapabilityKillCount;
 import targoss.hardcorealchemy.capability.killcount.ProviderKillCount;
 import targoss.hardcorealchemy.util.Chat;
+import targoss.hardcorealchemy.util.MobLists;
 
 public class ListenerPlayerMorph {
-	
-	public static Map<String, Integer> mapRequiredKills = new HashMap<String, Integer>();
-	
-	@CapabilityInject(ICapabilityKillCount.class)
+
+    public static Map<String, Integer> mapRequiredKills = new HashMap<String, Integer>();
+    public static Set<String> morphBlacklist = new HashSet<String>();
+
+    @CapabilityInject(ICapabilityKillCount.class)
     public static final Capability<ICapabilityKillCount> KILL_COUNT_CAPABILITY = null;
-	public static final ResourceLocation KILL_COUNT_RESOURCE_LOCATION = CapabilityKillCount.RESOURCE_LOCATION;
-	@CapabilityInject(ICapabilityHumanity.class)
+    public static final ResourceLocation KILL_COUNT_RESOURCE_LOCATION = CapabilityKillCount.RESOURCE_LOCATION;
+    @CapabilityInject(ICapabilityHumanity.class)
     public static final Capability<ICapabilityHumanity> HUMANITY_CAPABILITY = null;
-	
-	// The capability from Metamorph itself
+
+    // The capability from Metamorph itself
     @CapabilityInject(IMorphing.class)
     public static final Capability<IMorphing> MORPHING_CAPABILITY = null;
-	
-	static {
-	    //TODO: add the rest of the morph counts
-	    // Overworld animals
-	    mapRequiredKills.put("Chicken", 5);
-	    mapRequiredKills.put("Pig", 6);
-	    mapRequiredKills.put("Sheep", 6);
-	    mapRequiredKills.put("Cow", 6);
-	    mapRequiredKills.put("MushroomCow", 6);
-	    mapRequiredKills.put("PolarBear", 8);
-	    mapRequiredKills.put("Bat", 14);
-	    mapRequiredKills.put("Squid", 6);
-	    mapRequiredKills.put("Rabbit", 5);
-	    // Overworld animals (tameable)
-	    mapRequiredKills.put("Ozelot", 8);
-	    mapRequiredKills.put("EntityHorse", 12);
-	    mapRequiredKills.put("Villager", 8);
-	    // Overworld monsters
-	    mapRequiredKills.put("Slime", 6);
-	    mapRequiredKills.put("Silverfish", 8);
-	    mapRequiredKills.put("Spider", 8);
-	    mapRequiredKills.put("Zombie", 12);
-	    mapRequiredKills.put("Skeleton", 24);
-	    mapRequiredKills.put("Creeper", 12);
-	    mapRequiredKills.put("Enderman", 20);
-	    mapRequiredKills.put("Guardian", 8);
-	    // Nether monsters
-	    mapRequiredKills.put("LavaSlime", 14);
-	    mapRequiredKills.put("Blaze", 40);
-	    mapRequiredKills.put("Ghast", 8);
-	}
-	
-	public ListenerPlayerMorph() { }
-	
-	@SubscribeEvent
-	public void onAttachCapability(AttachCapabilitiesEvent<Entity> event) {
-	    Entity entity = event.getObject();
-	    if (!(entity instanceof EntityPlayerMP)) {
-	        return;
-	    }
-	    event.addCapability(KILL_COUNT_RESOURCE_LOCATION, new ProviderKillCount());
-	}
-	
-	@SubscribeEvent
+
+    static {
+        // TODO: add the rest of the morph counts
+        // Overworld animals
+        mapRequiredKills.put("Chicken", 5);
+        mapRequiredKills.put("Pig", 6);
+        mapRequiredKills.put("Sheep", 6);
+        mapRequiredKills.put("Cow", 6);
+        mapRequiredKills.put("MushroomCow", 6);
+        mapRequiredKills.put("PolarBear", 8);
+        mapRequiredKills.put("Bat", 14);
+        mapRequiredKills.put("Squid", 6);
+        mapRequiredKills.put("Rabbit", 5);
+        // Overworld animals (tameable)
+        mapRequiredKills.put("Ozelot", 8);
+        mapRequiredKills.put("EntityHorse", 12);
+        mapRequiredKills.put("Villager", 8);
+        // Overworld monsters
+        mapRequiredKills.put("Slime", 6);
+        mapRequiredKills.put("Silverfish", 8);
+        mapRequiredKills.put("Spider", 8);
+        mapRequiredKills.put("Zombie", 12);
+        mapRequiredKills.put("Skeleton", 24);
+        mapRequiredKills.put("Creeper", 12);
+        mapRequiredKills.put("Enderman", 20);
+        mapRequiredKills.put("Guardian", 8);
+        // Nether monsters
+        mapRequiredKills.put("LavaSlime", 14);
+        mapRequiredKills.put("Blaze", 40);
+        mapRequiredKills.put("Ghast", 8);
+
+        for (String human : MobLists.getHumans()) {
+            morphBlacklist.add(human);
+        }
+    }
+
+    @SubscribeEvent
+    public void onAttachCapability(AttachCapabilitiesEvent<Entity> event) {
+        Entity entity = event.getObject();
+        if (!(entity instanceof EntityPlayerMP)) {
+            return;
+        }
+        event.addCapability(KILL_COUNT_RESOURCE_LOCATION, new ProviderKillCount());
+    }
+
+    @SubscribeEvent
 	public void onSpawnGhost(SpawnGhostEvent.Pre event) {
 	    // Get morph
 		AbstractMorph morph = event.morph;
@@ -101,6 +108,12 @@ public class ListenerPlayerMorph {
 		if (morphName == null || morphName.equals("")) {
 		    return;
 		}
+		
+		// Prevent acquiring human-like morph
+		if (morphBlacklist.contains(EntityList.NAME_TO_CLASS.get(morphName))) {
+		    return;
+		}
+		
 		// Check if the player's humanity permits acquiring morphs
 		ICapabilityHumanity humanity = event.player.getCapability(HUMANITY_CAPABILITY, null);
 		if (humanity != null && !humanity.canMorph()) {
@@ -125,34 +138,34 @@ public class ListenerPlayerMorph {
 	        event.setCanceled(true);
 	    }
 	}
-	
-	@SubscribeEvent
-	public void onPlayerClone(Clone event) {
-	    if (event.isWasDeath() && Metamorph.proxy.config.keep_morphs) {
-	        // Keep kill count if morphs are kept
-	        EntityPlayer player = event.getEntityPlayer();
-	        EntityPlayer playerOld = event.getOriginal();
-	        CapUtil.copyOldToNew(KILL_COUNT_CAPABILITY, playerOld, player);
-	    }
-	}
-	
-	@Optional.Method(modid=HardcoreAlchemy.DISSOLUTION_ID)
-	@SubscribeEvent
-	public void onPlayerMorphAsGhost(MorphEvent.Pre event) {
-	    if (isIncorporeal(event.player) && !event.isDemorphing()) {
-    	        // You're a ghost, so being in a morph doesn't really make sense
-    	        event.setCanceled(true);
-    	        if (event.player instanceof EntityPlayerMP) {
-    	            Chat.notify((EntityPlayerMP)(event.player), "Your soul shall always be human.");
-	        }
-	    }
-	}
-	
-	@Optional.Method(modid=HardcoreAlchemy.DISSOLUTION_ID)
-	@SubscribeEvent
+
+    @SubscribeEvent
+    public void onPlayerClone(Clone event) {
+        if (event.isWasDeath() && Metamorph.proxy.config.keep_morphs) {
+            // Keep kill count if morphs are kept
+            EntityPlayer player = event.getEntityPlayer();
+            EntityPlayer playerOld = event.getOriginal();
+            CapUtil.copyOldToNew(KILL_COUNT_CAPABILITY, playerOld, player);
+        }
+    }
+
+    @Optional.Method(modid = HardcoreAlchemy.DISSOLUTION_ID)
+    @SubscribeEvent
+    public void onPlayerMorphAsGhost(MorphEvent.Pre event) {
+        if (isIncorporeal(event.player) && !event.isDemorphing()) {
+            // You're a ghost, so being in a morph doesn't really make sense
+            event.setCanceled(true);
+            if (event.player instanceof EntityPlayerMP) {
+                Chat.notify((EntityPlayerMP) (event.player), "Your soul shall always be human.");
+            }
+        }
+    }
+
+    @Optional.Method(modid = HardcoreAlchemy.DISSOLUTION_ID)
+    @SubscribeEvent
     public void onPlayerEnterAfterlife(PlayerRespawnEvent event) {
-	    EntityPlayer player = event.player;
-	    if (isIncorporeal(player)) {
+        EntityPlayer player = event.player;
+        if (isIncorporeal(player)) {
             IMorphing morphing = player.getCapability(MORPHING_CAPABILITY, null);
             if (morphing != null) {
                 // You're a ghost, so being in a morph doesn't really make sense
@@ -160,13 +173,14 @@ public class ListenerPlayerMorph {
             }
         }
     }
-	
-	@Optional.Method(modid=HardcoreAlchemy.DISSOLUTION_ID)
-	public boolean isIncorporeal(EntityPlayer player) {
-	    IIncorporealHandler incorporeal = player.getCapability(CapabilityIncorporealHandler.CAPABILITY_INCORPOREAL, null);
-	    if (incorporeal != null && incorporeal.isIncorporeal()) {
-	        return true;
-	    }
-	    return false;
-	}
+
+    @Optional.Method(modid = HardcoreAlchemy.DISSOLUTION_ID)
+    public boolean isIncorporeal(EntityPlayer player) {
+        IIncorporealHandler incorporeal = player.getCapability(CapabilityIncorporealHandler.CAPABILITY_INCORPOREAL,
+                null);
+        if (incorporeal != null && incorporeal.isIncorporeal()) {
+            return true;
+        }
+        return false;
+    }
 }
