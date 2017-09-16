ca5f12315cb9e0231d28499267cdabe8b54fa6e1
asanetargoss <asanetargoss@users.noreply.github.com>
2017-09-15 18:02:29 -0700

    Fix no-magic-crafting tooltip appearing regardless of the slot hovered over

--- src/main/java/targoss/hardcorealchemy/listener/ListenerGuiInventory.java
+++ src/main/java/targoss/hardcorealchemy/listener/ListenerGuiInventory.java
@@ -7,6 +7,11 @@ import static targoss.hardcorealchemy.listener.ListenerPlayerMagic.isAllowed;
 import java.util.List;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.gui.inventory.GuiInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.inventory.SlotCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
@@ -14,12 +19,15 @@ import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import targoss.hardcorealchemy.HardcoreAlchemy;
 import targoss.hardcorealchemy.capability.CapUtil;
 import targoss.hardcorealchemy.capability.food.ICapabilityFood;
 import targoss.hardcorealchemy.util.FoodLists;
 import targoss.hardcorealchemy.util.MorphDiet;
 
 public class ListenerGuiInventory {
+    private final Minecraft mc = Minecraft.getMinecraft();
+    
     @CapabilityInject(ICapabilityFood.class)
     public static final Capability<ICapabilityFood> FOOD_CAPABILITY = null;
     
@@ -47,6 +55,12 @@ public class ListenerGuiInventory {
     // A client-side tooltip when hovering over an uncraftable magic item
     @SubscribeEvent
     public void onTooltipMagicCrafting(ItemTooltipEvent event) {
+        GuiScreen gui = mc.currentScreen;
+        if (gui == null ||
+                !(gui instanceof GuiContainer) ||
+                !(((GuiContainer)gui).theSlot instanceof SlotCrafting)) {
+            return;
+        }
         ItemStack craftResult = event.getItemStack();
         if (!canUseHighMagic && !isAllowed(MAGIC_ITEM_ALLOW_CRAFT, craftResult)) {
             event.getToolTip().add(TextFormatting.DARK_GRAY.toString() + new TextComponentTranslation("hardcorealchemy.magic.disabled.crafttooltip").getUnformattedText());
