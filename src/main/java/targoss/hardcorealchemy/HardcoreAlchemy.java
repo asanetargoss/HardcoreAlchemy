9aa8c9c2c4aea43b228284be3e0cdb977b2441b1
asanetargoss <asanetargoss@users.noreply.github.com>
2018-01-13 00:10:29 -0800

    Version 0.3.0

--- src/main/java/targoss/hardcorealchemy/HardcoreAlchemy.java
+++ src/main/java/targoss/hardcorealchemy/HardcoreAlchemy.java
@@ -44,7 +44,7 @@ import targoss.hardcorealchemy.test.HardcoreAlchemyTests;
 public class HardcoreAlchemy
 {
     public static final String MOD_ID = "hardcorealchemy";
-    public static final String VERSION = "0.2.1";
+    public static final String VERSION = "0.3.0";
     public static final String DEPENDENCIES = "required-after:metamorph;" +
             "after:astralsorcery;" +
             "after:adinferos;" +
