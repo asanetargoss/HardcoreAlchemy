8c60b621aabf3783c0057ed5a66333f37f3e8bb8
asanetargoss <asanetargoss@users.noreply.github.com>
2018-01-12 23:00:54 -0800

    Add LGPL license

--- src/main/java/targoss/hardcorealchemy/capability/killcount/ProviderKillCount.java
+++ src/main/java/targoss/hardcorealchemy/capability/killcount/ProviderKillCount.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2017-2018 asanetargoss
+ * 
+ * This file is part of Hardcore Alchemy.
+ * 
+ * Hardcore Alchemy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation version 3 of the License.
+ * 
+ * Hardcore Alchemy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Hardcore Alchemy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package targoss.hardcorealchemy.capability.killcount;
 
 import net.minecraft.nbt.NBTBase;
