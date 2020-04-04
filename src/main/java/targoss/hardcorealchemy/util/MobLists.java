ef639da1412b12eaa7c0706580cef9fe9c99deb2
asanetargoss <asanetargoss@users.noreply.github.com>
2020-04-03 23:01:13 -0700

    Clean up MobLists includes

--- src/main/java/targoss/hardcorealchemy/util/MobLists.java
+++ src/main/java/targoss/hardcorealchemy/util/MobLists.java
@@ -21,32 +21,6 @@ package targoss.hardcorealchemy.util;
 import java.util.HashSet;
 import java.util.Set;
 
-import mchorse.metamorph.entity.EntityMorph;
-import net.minecraft.entity.EntityList;
-import net.minecraft.entity.boss.EntityDragon;
-import net.minecraft.entity.boss.EntityWither;
-import net.minecraft.entity.monster.EntityBlaze;
-import net.minecraft.entity.monster.EntityCreeper;
-import net.minecraft.entity.monster.EntityEnderman;
-import net.minecraft.entity.monster.EntityGhast;
-import net.minecraft.entity.monster.EntityMagmaCube;
-import net.minecraft.entity.monster.EntityPigZombie;
-import net.minecraft.entity.monster.EntitySilverfish;
-import net.minecraft.entity.monster.EntitySkeleton;
-import net.minecraft.entity.monster.EntitySlime;
-import net.minecraft.entity.monster.EntitySpider;
-import net.minecraft.entity.monster.EntityWitch;
-import net.minecraft.entity.monster.EntityZombie;
-import net.minecraft.entity.passive.EntityBat;
-import net.minecraft.entity.passive.EntityChicken;
-import net.minecraft.entity.passive.EntityCow;
-import net.minecraft.entity.passive.EntityHorse;
-import net.minecraft.entity.passive.EntityOcelot;
-import net.minecraft.entity.passive.EntityPig;
-import net.minecraft.entity.passive.EntitySheep;
-import net.minecraft.entity.passive.EntityVillager;
-import net.minecraft.entity.passive.EntityWolf;
-import targoss.hardcorealchemy.HardcoreAlchemy;
 import targoss.hardcorealchemy.entity.Entities;
 
 /**
