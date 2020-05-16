addeabc31c871b53f5eee1072a7486c107be4d89
asanetargoss <asanetargoss@users.noreply.github.com>
2020-05-16 09:45:21 -0700

    (WIP) Nether instinct implementation

--- src/main/java/targoss/hardcorealchemy/instinct/InstinctEffectTemperedFlame.java
+++ src/main/java/targoss/hardcorealchemy/instinct/InstinctEffectTemperedFlame.java
@@ -18,15 +18,29 @@
 
 package targoss.hardcorealchemy.instinct;
 
+import java.util.Random;
+import java.util.function.Predicate;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.DimensionType;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
 import targoss.hardcorealchemy.capability.instinct.ICapabilityInstinct;
 import targoss.hardcorealchemy.instinct.api.IInstinctEffectData;
 import targoss.hardcorealchemy.instinct.api.InstinctEffect;
 import targoss.hardcorealchemy.listener.ListenerInstinctOverheat;
+import targoss.hardcorealchemy.util.WorldUtil;
+import targoss.hardcorealchemy.util.WorldUtil.BlockInfo;
 
 /** Player is too cold to function unless regularly exposed to flame */
 public class InstinctEffectTemperedFlame extends InstinctEffect {
@@ -45,6 +59,7 @@ public class InstinctEffectTemperedFlame extends InstinctEffect {
     public static final float COLD_PER_TICK_PER_AMP = COLD_PER_DAY_PER_AMP / 12000F;
 
     protected static class Data implements IInstinctEffectData {
+        public Random random = new Random();
         /** Larger means cold effects will be worse (measured in ticks) */
         public int coolingTime;
         public int maxCoolingTime;
@@ -99,12 +114,21 @@ public class InstinctEffectTemperedFlame extends InstinctEffect {
             return 0.0F;
         }
         float baseColdAmplifier = 0.0F;
-        /*
         if (amplifier >= 0.5F) {
-            if (standing on/in ice/snow) {
-                baseColdAmplifier = Math.max(baseColdAmplifier, amplifier * 1.0F);
+            BlockPos steppingPos = new BlockPos((int)player.posX, (int)Math.floor(player.posY - 0.2), (int)player.posZ);
+            IBlockState steppingState = player.world.getBlockState(steppingPos);
+            if (steppingState != null) {
+                Block steppingBlock = steppingState.getBlock();
+                //TODO
+                /*
+                if (standing on/in ice/snow) {
+                    baseColdAmplifier = Math.max(baseColdAmplifier, amplifier * 1.0F);
+                }
+                */
             }
         }
+        //TODO
+        /*
         if (amplifier >= 1.25F) {
             if (in water) {
                 baseColdAmplifier = Math.max(baseColdAmplifier, amplifier * 0.9F);
@@ -128,15 +152,62 @@ public class InstinctEffectTemperedFlame extends InstinctEffect {
     }
     
     public static void applyColdEffects(EntityPlayer player, float amplifier) {
-        // TODO: Potion effects I guess. Severity/quantity depends on amplifier. Timer is probably the same regardless, probably short (10 seconds)
-        // player.addPotionEffect(slowness)
-        // player.addPotionEffect(weakness)
-        // player.addPotionEffect(mining_fatigue)
-        // player.addPotionEffect(slowness2)
+        final int effectTime = 10 * 20;
+        if (amplifier >= 0.5F) {
+            if (amplifier >= 2.0F) {
+                // Slowness II
+                player.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, effectTime, 1));
+            }
+            else {
+                player.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, effectTime));
+            }
+        }
+        if (amplifier >= 1.0F) {
+            player.addPotionEffect(new PotionEffect(MobEffects.WEAKNESS, effectTime));
+        }
+        if (amplifier >= 1.5F) {
+            player.addPotionEffect(new PotionEffect(MobEffects.MINING_FATIGUE, effectTime));
+        }
     }
 
-    protected static void consumeLesserHeat(EntityPlayer player) {
-        // TODO: Chance to consume fire/lava source while NOT in Nether (may consume block if fire burns eternally on it)
+    protected static void consumeLesserHeat(EntityPlayer player, Data data) {
+        // Consume fire/lava source/fire fuel while NOT in Nether
+        // A fire block will always be consumed if in range. Fire fuel/lava have a chance to be consumed.
+        // The player will also no longer be on fire (even if in the Nether)
+        if (player.dimension != DimensionType.NETHER.getId()) {
+            if (player.onGround) {
+                BlockInfo fireInfo = WorldUtil.getRandomCollidingState(data.random, player, new Predicate<BlockInfo>() {
+                    @Override
+                    public boolean test(BlockInfo info) {
+                        return info.getState().getBlock() == net.minecraft.init.Blocks.FIRE;
+                    }
+                }, WorldUtil.CollisionMethod.FIRE);
+                if (fireInfo != null) {
+                    player.world.setBlockToAir(fireInfo.getPos());
+                    BlockPos fuelPos = WorldUtil.getFireFuelPos(player.world, fireInfo.getPos());
+                    if (data.random.nextFloat() <= 0.25F) {
+                        player.world.setBlockToAir(fuelPos);
+                    }
+                }
+            }
+
+            BlockInfo lavaInfo = WorldUtil.getRandomCollidingState(data.random, player, new Predicate<BlockInfo>() {
+                @SuppressWarnings("deprecation")
+                @Override
+                public boolean test(BlockInfo info) {
+                    return info.getState().getBlock().getMaterial(info.getState()) == Material.LAVA;
+                }
+            }, WorldUtil.CollisionMethod.LAVA);
+            if (lavaInfo != null) {
+                if (data.random.nextFloat() <= 0.05F) {
+                    player.world.setBlockToAir(lavaInfo.getPos());
+                }
+            }
+        }
+
+        player.extinguish();
+        player.world.playSound(null, player.getPosition(), SoundEvents.BLOCK_LAVA_EXTINGUISH, SoundCategory.BLOCKS,
+                0.5F, 2.6F + (data.random.nextFloat() - data.random.nextFloat()) * 0.8F);
     }
     
     public static boolean needsHeat(Data data) {
@@ -145,7 +216,7 @@ public class InstinctEffectTemperedFlame extends InstinctEffect {
     }
     
     protected static void exposeToHeat(EntityPlayer player, Data data) {
-        // TODO: Message that the player did something good (but not good enough)
+        // TODO: Message that the player did something good (for now)
         data.coolingTime = 0;
     }
 
@@ -187,14 +258,13 @@ public class InstinctEffectTemperedFlame extends InstinctEffect {
         Data data = (Data)instinct.getInstinctEffectData(this);
         if (InstinctEffectNetherFever.isInHeat(player)) {
             if (needsHeat(data)) {
-                consumeLesserHeat(player);
+                consumeLesserHeat(player, data);
             }
             exposeToHeat(player, data);
         } else if (!ListenerInstinctOverheat.isOverheating(player)) {
             float coldAmplifier = getColdAmplifier(player, data, amplifier);
             applyColdEffects(player, coldAmplifier);
         }
-        // TODO Auto-generated method stub
         data.updateTimers();
     }
 
---
+++ src/main/java/targoss/hardcorealchemy/util/WorldUtil.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2020 asanetargoss
+ * 
+ * This file is part of Hardcore Alchemy.
+ * 
+ * Hardcore Alchemy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation version 3 of the License.
+ * 
+ * Hardcore Alchemy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Hardcore Alchemy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package targoss.hardcorealchemy.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Predicate;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+
+public class WorldUtil {
+    public static enum CollisionMethod {
+        FULL,
+        FIRE,
+        LAVA
+    }
+    
+    public static class BlockInfo {
+        protected BlockPos pos;
+        protected IBlockState state;
+        public BlockInfo(BlockPos pos, IBlockState state) {
+            this.pos = pos.toImmutable();
+            this.state = state;
+        }
+        public BlockInfo(MutableBlockInfo other) {
+            this.pos = other.pos.toImmutable();
+            this.state = other.state;
+        }
+        public BlockPos getPos() {
+            return pos;
+        }
+        public IBlockState getState() {
+            return state;
+        }
+        public BlockInfo toImmutable() {
+            return this;
+        }
+    }
+    
+    public static class MutableBlockInfo extends BlockInfo {
+        public MutableBlockInfo() {
+            super(null, null);
+        }
+        public MutableBlockInfo(BlockPos pos, IBlockState state) {
+            super(pos, state);
+        }
+        public void setPos(BlockPos pos) {
+            this.pos = pos;
+        }
+        public void setState(IBlockState state) {
+            this.state = state;
+        }
+        @Override
+        public BlockInfo toImmutable() {
+            return new BlockInfo(this);
+        }
+    }
+    
+    /**
+     * Ignores air/anything null.
+     * Need CollisionMethod because Minecraft is inconsistent about colliding with things.
+     * */
+    public static List<BlockInfo> getCollidingStates(Entity entity, Predicate<BlockInfo> predicate, CollisionMethod collisionMethod) {
+        List<BlockInfo> matching = new ArrayList<>(); 
+        
+        AxisAlignedBB box;
+        switch (collisionMethod) {
+        case FIRE:
+            box = entity.getEntityBoundingBox().contract(0.001);
+            break;
+        case LAVA:
+            box = entity.getEntityBoundingBox().expand(-0.1, -0.4, -0.1);
+            break;
+        default:
+            box = entity.getEntityBoundingBox();
+            break;
+        }
+        
+        int xMin = MathHelper.floor(box.minX);
+        int xMax = MathHelper.ceil(box.maxX);
+        int yMin = MathHelper.floor(box.minY);
+        int yMax = MathHelper.ceil(box.maxY);
+        int zMin = MathHelper.floor(box.minZ);
+        int zMax = MathHelper.ceil(box.maxZ);
+        BlockPos.PooledMutableBlockPos mPos = BlockPos.PooledMutableBlockPos.retain();
+        MutableBlockInfo blockInfo = new MutableBlockInfo();
+        for (int x = xMin; x < xMax; ++x) {
+            for (int y = yMin; y < yMax; ++y) {
+                for (int z = zMin; z < zMax; ++z) {
+                    IBlockState state = entity.world.getBlockState(mPos.setPos(x, y, z));
+                    if (state == null) {
+                        continue;
+                    }
+                    Block block = state.getBlock();
+                    if (block == null || block.isAir(state, entity.world, mPos)) {
+                        continue;
+                    }
+                    blockInfo.setPos(mPos);
+                    blockInfo.setState(state);
+                    if (predicate.test(blockInfo)) {
+                        matching.add(blockInfo.toImmutable());
+                    }
+                }
+            }
+        }
+        mPos.release();
+        
+        return matching;
+    }
+    
+    public static @Nullable BlockInfo getRandomCollidingState(Random random, Entity entity, Predicate<BlockInfo> predicate, CollisionMethod collisionMethod) {
+        List<BlockInfo> matching = getCollidingStates(entity, predicate, collisionMethod);
+        if (matching.isEmpty()) {
+            return null;
+        }
+        return matching.get(random.nextInt(matching.size()));
+    }
+    
+    public static @Nullable BlockPos getFireFuelPos(World world, BlockPos firePos) {
+        BlockPos burnPos;
+        burnPos = firePos.down();
+        if (Blocks.FIRE.canCatchFire(world, burnPos, EnumFacing.UP)) {
+            return burnPos;
+        }
+        burnPos = firePos.west();
+        if (Blocks.FIRE.canCatchFire(world, burnPos, EnumFacing.EAST)) {
+            return burnPos;
+        }
+        burnPos = firePos.east();
+        if (Blocks.FIRE.canCatchFire(world, burnPos, EnumFacing.WEST)) {
+            return burnPos;
+        }
+        burnPos = firePos.north();
+        if (Blocks.FIRE.canCatchFire(world, burnPos, EnumFacing.SOUTH)) {
+            return burnPos;
+        }
+        burnPos = firePos.south();
+        if (Blocks.FIRE.canCatchFire(world, burnPos, EnumFacing.NORTH)) {
+            return burnPos;
+        }
+        burnPos = firePos.up();
+        if (Blocks.FIRE.canCatchFire(world, burnPos, EnumFacing.DOWN)) {
+            return burnPos;
+        }
+        return null;
+    }
+}
