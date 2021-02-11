e7a58b9a949bc896bfe647e88935efd94f0051fa
asanetargoss <asanetargoss@users.noreply.github.com>
2021-02-10 21:11:28 -0800

    Compensate for loss of thirst penalty when ThirstStatHandler.onPlayerAttackEntity is removed

--- src/main/java/targoss/hardcorealchemy/listener/ListenerSmallTweaks.java
+++ src/main/java/targoss/hardcorealchemy/listener/ListenerSmallTweaks.java
@@ -26,6 +26,7 @@ import ca.wescook.nutrition.capabilities.CapProvider;
 import ca.wescook.nutrition.nutrients.Nutrient;
 import net.minecraft.block.BlockBed;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
@@ -41,6 +42,7 @@ import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.entity.living.ZombieEvent;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Optional;
 import net.minecraftforge.fml.common.eventhandler.Event.Result;
@@ -54,7 +56,10 @@ import targoss.hardcorealchemy.item.Items;
 import targoss.hardcorealchemy.util.Chat;
 import targoss.hardcorealchemy.util.InventoryUtil;
 import targoss.hardcorealchemy.util.MiscVanilla;
+import toughasnails.api.TANCapabilities;
 import toughasnails.api.TANPotions;
+import toughasnails.handler.thirst.ThirstStatHandler;
+import toughasnails.thirst.ThirstHandler;
 
 /**
  * An event listener for miscellaneous changes that
@@ -151,6 +156,45 @@ public class ListenerSmallTweaks extends ConfiguredListener {
             }
         }
         
+    }
+    
+    protected Boolean tanAttackHandlerExists = null;
+    protected boolean doesTanAttackHandlerExist() {
+        if (tanAttackHandlerExists != null) {
+            return tanAttackHandlerExists;
+        }
+        try {
+            ThirstStatHandler handler = new ThirstStatHandler();
+            handler.onPlayerAttackEntity(null);
+        } catch (NoSuchMethodError e) {
+            tanAttackHandlerExists = false;
+            return tanAttackHandlerExists;
+        } catch (NullPointerException e) {}
+        tanAttackHandlerExists = true;
+        return tanAttackHandlerExists;
+    }
+
+    /**
+     * Simpler implementation of TaN's ThirstStatHandler.onPlayerAttackEntity which does not
+     * break mob death sounds. However, it only fires if ThirstStatHandler.onPlayerAttackEntity
+     * has been removed from the code, for example via killitwithfire.
+     * */
+    @SubscribeEvent
+    @Optional.Method(modid=ModState.TAN_ID)
+    public void onTanThirstAttack(AttackEntityEvent event) {
+        if (doesTanAttackHandlerExist()) {
+            return;
+        }
+        EntityPlayer player = event.getEntityPlayer();
+        if (player.world.isRemote) {
+            return;
+        }
+        Entity target = event.getTarget();
+        if (!target.canBeAttackedWithItem() || target.hitByEntity(player)) {
+            return;
+        }
+        ThirstHandler thirst = (ThirstHandler)player.getCapability(TANCapabilities.THIRST, null);
+        thirst.addExhaustion(0.3F);
     }
     
     @Optional.Method(modid=ModState.NUTRITION_ID)
