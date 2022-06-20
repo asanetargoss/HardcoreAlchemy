45b846508668dc42595d375e4127dc11174b2da4
asanetargoss <asanetargoss@users.noreply.github.com>
2022-06-20 14:12:08 -0700

    Sort mods in ModState alphabetically

--- core/src/main/java/targoss/hardcorealchemy/ModState.java
+++ core/src/main/java/targoss/hardcorealchemy/ModState.java
@@ -27,36 +27,36 @@ import net.minecraftforge.fml.common.ModContainer;
  * Note: Not all ids have a corresponding boolean
  */
 public final class ModState {
-    public static final String METAMORPH_ID = "metamorph";
-    public static final String DISSOLUTION_ID = "dissolution";
-    public static final String NUTRITION_ID = "nutrition";
-    public static final String BLOOD_MAGIC_ID = "BloodMagic";
-    public static final String ARS_MAGICA_ID = "arsmagica2";
-    public static final String PROJECT_E_ID = "ProjectE";
-    public static final String TAN_ID = "ToughAsNails";
-    public static final String HARVESTCRAFT_ID = "harvestcraft";
     public static final String ADINFEROS_ID = "adinferos";
-    public static final String THAUMCRAFT_ID = "thaumcraft";
-    public static final String DEADLY_MONSTERS_ID = "dmonsters";
     public static final String ALCHEMIC_ASH_ID = "AlchemicAsh";
-    public static final String JEI_ID = "JEI";
-    public static final String WAWLA_ID = "Waila";
-    public static final String SPICE_OF_LIFE_ID = "SpiceOfLife";
-    public static final String MANTLE_ID = "mantle";
+    public static final String ARS_MAGICA_ID = "arsmagica2";
+    public static final String BLOOD_MAGIC_ID = "BloodMagic";
+    public static final String DEADLY_MONSTERS_ID = "dmonsters";
+    public static final String DISSOLUTION_ID = "dissolution";
     public static final String ENDER_ZOO_ID = "EnderZoo";
-    public static boolean isMetamorphLoaded = false;
-    public static boolean isDissolutionLoaded = false;
-    public static boolean isNutritionLoaded = false;
-    public static boolean isBloodMagicLoaded = false;
-    public static boolean isArsMagicaLoaded = false;
-    public static boolean isProjectELoaded = false;
-    public static boolean isTanLoaded = false;
-    public static boolean isHarvestCraftLoaded = false;
-    public static boolean isThaumcraftLoaded = false;
+    public static final String HARVESTCRAFT_ID = "harvestcraft";
+    public static final String JEI_ID = "JEI";
+    public static final String MANTLE_ID = "mantle";
+    public static final String METAMORPH_ID = "metamorph";
+    public static final String NUTRITION_ID = "nutrition";
+    public static final String PROJECT_E_ID = "ProjectE";
+    public static final String SPICE_OF_LIFE_ID = "SpiceOfLife";
+    public static final String TAN_ID = "ToughAsNails";
+    public static final String THAUMCRAFT_ID = "thaumcraft";
+    public static final String WAWLA_ID = "Waila";
     public static boolean isAlchemicAshLoaded = false;
+    public static boolean isArsMagicaLoaded = false;
+    public static boolean isBloodMagicLoaded = false;
+    public static boolean isDissolutionLoaded = false;
+    public static boolean isHarvestCraftLoaded = false;
     public static boolean isJEILoaded = false;
-    public static boolean isSpiceOfLifeLoaded = false;
     public static boolean isMantleLoaded = false;
+    public static boolean isMetamorphLoaded = false;
+    public static boolean isNutritionLoaded = false;
+    public static boolean isProjectELoaded = false;
+    public static boolean isSpiceOfLifeLoaded = false;
+    public static boolean isTanLoaded = false;
+    public static boolean isThaumcraftLoaded = false;
     
     protected static boolean modMapRegistered = false;
 
@@ -68,18 +68,18 @@ public final class ModState {
         }
         modMapRegistered = true;
         
-        isMetamorphLoaded = modMap.containsKey(METAMORPH_ID);
-        isDissolutionLoaded = modMap.containsKey(DISSOLUTION_ID);
-        isNutritionLoaded = modMap.containsKey(NUTRITION_ID);
-        isBloodMagicLoaded = modMap.containsKey(BLOOD_MAGIC_ID);
-        isArsMagicaLoaded = modMap.containsKey(ARS_MAGICA_ID);
-        isProjectELoaded = modMap.containsKey(PROJECT_E_ID);
-        isTanLoaded = modMap.containsKey(TAN_ID);
-        isHarvestCraftLoaded = modMap.containsKey(HARVESTCRAFT_ID);
-        isThaumcraftLoaded = modMap.containsKey(THAUMCRAFT_ID);
         isAlchemicAshLoaded = modMap.containsKey(ALCHEMIC_ASH_ID);
+        isArsMagicaLoaded = modMap.containsKey(ARS_MAGICA_ID);
+        isBloodMagicLoaded = modMap.containsKey(BLOOD_MAGIC_ID);
+        isDissolutionLoaded = modMap.containsKey(DISSOLUTION_ID);
+        isHarvestCraftLoaded = modMap.containsKey(HARVESTCRAFT_ID);
         isJEILoaded = modMap.containsKey(JEI_ID);
-        isSpiceOfLifeLoaded = modMap.containsKey(SPICE_OF_LIFE_ID);
         isMantleLoaded = modMap.containsKey(MANTLE_ID);
+        isMetamorphLoaded = modMap.containsKey(METAMORPH_ID);
+        isNutritionLoaded = modMap.containsKey(NUTRITION_ID);
+        isProjectELoaded = modMap.containsKey(PROJECT_E_ID);
+        isSpiceOfLifeLoaded = modMap.containsKey(SPICE_OF_LIFE_ID);
+        isTanLoaded = modMap.containsKey(TAN_ID);
+        isThaumcraftLoaded = modMap.containsKey(THAUMCRAFT_ID);
     }
 }
