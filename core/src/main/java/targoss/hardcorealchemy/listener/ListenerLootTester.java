d0e3f2acd12a8f32077571548b2e6c71845ded82
asanetargoss <asanetargoss@users.noreply.github.com>
2023-05-07 14:47:22 -0700

    Update licenses

--- core/src/main/java/targoss/hardcorealchemy/listener/ListenerLootTester.java
+++ core/src/main/java/targoss/hardcorealchemy/listener/ListenerLootTester.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2017-2023 asanetargoss
+ *
+ * This file is part of Hardcore Alchemy Core.
+ *
+ * Hardcore Alchemy Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3 or, at
+ * your option, any later version of the GNU Lesser General Public License as
+ * published by the Free Software Foundation.
+ *
+ * Hardcore Alchemy Core is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with Hardcore Alchemy Core. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package targoss.hardcorealchemy.listener;
 
 import net.minecraftforge.event.ServerChatEvent;
